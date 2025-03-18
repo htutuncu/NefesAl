f7abd694631759cdeab084033c4dd90528f606b3
Hikmet Tutuncu <48223977+htutuncu@users.noreply.github.com>
2025-03-18 14:47:47 +0300

    Delete app/build directory

--- app/build/generated/hilt/component_trees/release/dagger/hilt/internal/processedrootsentinel/codegen/_com_agonlabs_nefesal_NefesAlApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.agonlabs.nefesal.NefesAlApplication"
-)
-public final class _com_agonlabs_nefesal_NefesAlApplication {
-}
